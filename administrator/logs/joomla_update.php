9876313a88b45163b345cd578e83228a4096053f
Tejashri Majage <tejashri_m@techjoomla.com>
2022-10-19 18:52:40 +0530

    Joomla update to version 4.2.3

--- administrator/logs/joomla_update.php
+++ administrator/logs/joomla_update.php
@@ -6,3 +6,7 @@
 #Fields: datetime	priority clientip	category	message
 2022-10-19T12:56:39+00:00	INFO 127.0.0.1	update	Starting installation of new version.
 2022-10-19T12:57:49+00:00	INFO 127.0.0.1	update	Starting installation of new version.
+2022-10-19T13:14:08+00:00	INFO 127.0.0.1	update	Update started by user Tejashree (3). Old version is 4.1.5.
+2022-10-19T13:14:11+00:00	INFO 127.0.0.1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla4/Joomla_4.2.3-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20221019%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221019T131404Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=c41c1152f5c9c47f27f397297419879e50192b1870b55828b62a232bb8a9e290.
+2022-10-19T13:16:12+00:00	INFO 127.0.0.1	update	Starting installation of new version.
+2022-10-19T13:16:45+00:00	INFO 127.0.0.1	update	File Joomla_4.2.3-Stable-Update_Package.zip downloaded.
